ef7e493af3dc8a14025d8125f344b049cf7a9bc5
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-01-20 21:18:38 +0300

    Add evaluation.

--- micro.py
+++ micro.py
@@ -16,12 +16,24 @@ def get_code():
 def get_tokens(code):
 	return code.split(' ')
 
+def evaluate(tokens):
+	name = tokens[0]
+	if name not in functions:
+		return int(name), 1
+
+	function = functions[name]
+	arguments = []
+	offset = 1
+	for _ in xrange(function[0]):
+		value, shift = evaluate(tokens[offset:])
+		arguments.append(value)
+		offset += shift
+
+	value = function[1](*arguments)
+	return value, 1 + len(arguments)
+
 if __name__ == '__main__':
 	code = get_code()
 	tokens = get_tokens(code)
-	print(tokens)
-
-	print(functions['+'][1](2, 3))
-	print(functions['-'][1](2, 3))
-	print(functions['*'][1](2, 3))
-	print(functions['/'][1](2, 3))
+	value, _ = evaluate(tokens)
+	print(value)
