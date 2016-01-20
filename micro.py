de397151ed22b419970f1f4b53748c0c81dee78e
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-01-20 20:41:46 +0300

    Add function map.

--- micro.py
+++ micro.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 from sys import argv
+from operator import add, sub, mul, div
+
+functions = { \
+	'+': (2, add), \
+	'-': (2, sub), \
+	'*': (2, mul), \
+	'/': (2, div) \
+}
 
 def get_code():
 	return argv[1]
@@ -12,3 +20,8 @@ if __name__ == '__main__':
 	code = get_code()
 	tokens = get_tokens(code)
 	print(tokens)
+
+	print(functions['+'][1](2, 3))
+	print(functions['-'][1](2, 3))
+	print(functions['*'][1](2, 3))
+	print(functions['/'][1](2, 3))
