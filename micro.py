6623d5679eef2c3db70edce7334582b5d524786d
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-01-23 18:27:08 +0300

    Add a detection of a function definition.

--- micro.py
+++ micro.py
@@ -16,9 +16,17 @@ def get_code():
 def get_tokens(code):
 	return code.split(' ')
 
+def parse_function(tokens):
+	return 'test', (23, None), tokens[-1:]
+
 def evaluate(tokens):
 	name = tokens[0]
 	tokens = tokens[1:]
+	if name == 'fn':
+		name, function, tokens = parse_function(tokens)
+		functions[name] = function
+
+		return 0, tokens
 	if name not in functions:
 		return int(name), tokens
 
@@ -36,3 +44,4 @@ if __name__ == '__main__':
 	tokens = get_tokens(code)
 	value, _ = evaluate(tokens)
 	print(value)
+	print(functions)
