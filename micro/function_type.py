5ed65efee00f10d2d5272b2d778ca9454df298f7
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-07-21 05:19:00 +0300

    Add the third snapshot of the code

--- micro/function_type.py
+++ micro/function_type.py
@@ -1,11 +1,17 @@
+import ast_node
+
 class FunctionType:
     def __init__(self, arity=0, result=None):
         self.arity = arity
         self.result = result
 
+    def get_result(self):
+        return self.result if self.result is not None else FunctionType(0)
+
     def to_ast(self):
         arities = self.to_array()
-        pass
+        arities_nodes = [ast_node.AstNode('arity', value=arity) for arity in arities]
+        return ast_node.AstNode('type', children=arities_nodes)
 
     def to_array(self):
         return _simplify_type(self._to_raw_array())
@@ -13,8 +19,11 @@ class FunctionType:
     def _to_raw_array(self):
         return [self.arity] + (self.result._to_raw_array() if self.result is not None else [])
 
-def make_type_from_array(arities):
-    return _simplify_type(_make_type_from_raw_array(arities))
+def make_type(value):
+    if isinstance(value, ast_node.AstNode):
+        return _make_type_from_ast(value)
+    else:
+        return _make_type_from_array(value)
 
 def _simplify_type(arities):
     first_nonzero = next((index for index, arity in zip(range(len(arities), 0, -1), reversed(arities)) if arity != 0), -1)
@@ -22,6 +31,13 @@ def _simplify_type(arities):
         return []
 
     return arities[:first_nonzero]
+
+def _make_type_from_ast(type_node):
+    arities = [arity_node.value for arity_node in type_node.children]
+    return _make_type_from_array(arities)
+
+def _make_type_from_array(arities):
+    return _make_type_from_raw_array(_simplify_type(arities))
 
 def _make_type_from_raw_array(arities):
     if len(arities) == 0:
--- micro/parser.py
+++ micro/parser.py
@@ -9,104 +9,70 @@ class Parser:
         self._functions = functions
 
     def parse(self, preast):
-        preast.children = self._make_call_list(preast.children, self._functions)
+        preast.children = self._make_calls(preast.children, self._functions)
         return preast
 
     def get_errors(self):
         return self._errors
 
-    def _make_call_list(self, entities, functions):
+    def _make_calls(self, entities, functions):
         calls = []
         while len(entities) > 0:
             call, entities = self._make_call(entities[0], entities[1:], functions)
             if call is None:
                 continue
-            if call.name == 'function':
-                call.children[1].children = self._make_call_list(call.children[1].children, functions)
-            if call.name == 'call' and call.children[1].children[-1].name == 'closure_mark':
-                call.children[1].children.pop()
-                call = self._make_closure_node(call, functions)
 
             calls.append(call)
 
         return calls
 
     def _make_call(self, first_entity, rest_entities, functions):
-        arity = self._get_arity(first_entity, functions)
-        if arity is None:
+        entity_type = self._get_type(first_entity, functions)
+        if entity_type.arity is None:
             return None, rest_entities
 
         arguments = []
+        arity = entity_type.arity
         while arity > 0 and len(rest_entities) > 0:
             call, rest_entities = self._make_call(rest_entities[0], rest_entities[1:], functions)
             if call is None:
                 continue
-            if call.name == 'closure_mark':
-                break
 
             arguments.append(call)
             arity -= 1
-
         if arity > 0:
-            arguments.append(ast_node.AstNode('closure_mark'))
+            self._errors.append(error.Error('{}'.format(utilities.quote('dummy')), 0))
+            return None, rest_entities
 
         if len(arguments) == 0:
+            if first_entity.name == 'function':
+                first_entity.children[1].children = self._make_calls(first_entity.children[1].children, functions)
+
             return first_entity, rest_entities
         else:
-            call = self._make_call_node(first_entity, arguments)
+            call = self._make_call_node(first_entity, entity_type, arguments)
             return self._make_call(call, rest_entities, functions)
 
-    def _get_arity(self, entity, functions):
-        arity = 0
+    def _get_type(self, entity, functions):
+        entity_type = function_type.make_type([])
         if entity.name == 'IDENTIFIER':
             if entity.value in self._functions:
-                arity = functions[entity.value]
+                entity_type = functions[entity.value]
             else:
                 self._errors.append(error.Error('unknown function {}'.format(utilities.quote(entity.value)), entity.offset))
-                arity = None
+                entity_type = None
         elif entity.name == 'call':
-            pass
+            entity_type = function_type.make_type(entity.children[0].children[1].children[0])
 
-        return arity
+        return entity_type
 
-    def _make_call_node(self, function, arguments):
-        inner_call_node = ast_node.AstNode('inner_call', children=[function])
-        result_node = self._make_result_node(0)
+    def _make_call_node(self, entity, entity_type, arguments):
+        inner_call_node = ast_node.AstNode('inner_call', children=[entity])
+        type_node = entity_type.get_result().to_ast()
+        result_node = ast_node.AstNode('result', children=[type_node])
         inner_function_node = ast_node.AstNode('inner_function', children=[inner_call_node, result_node])
         arguments_node = ast_node.AstNode('argument_list', children=arguments)
         return ast_node.AstNode('call', children=[inner_function_node, arguments_node])
-
-    def _make_closure_node(self, call, functions):
-        arguments_names = self._get_closure_arguments_names(call, functions)
-        closure_declaration_node = self._make_closure_declaration_node(arguments_names)
-        closure_body_node = self._make_closure_body_node(call, arguments_names)
-        return ast_node.AstNode('function', children=[closure_declaration_node, closure_body_node])
-
-    def _get_closure_arguments_names(self, call, functions):
-        call_arity = functions[call.children[0].value]
-        closure_arity = call_arity - len(call.children[1].children)
-        return ['_{}'.format(i) for i in range(closure_arity)]
-
-    def _make_closure_declaration_node(self, arguments_names):
-        name_node = ast_node.AstNode('name', value='')
-        arguments_node = self._make_closure_arguments_node(arguments_names)
-        result_node = self._make_closure_result_node(len(arguments_names))
-        return ast_node.AstNode('function_declaration', children=[name_node, arguments_node, result_node])
-
-    def _make_closure_arguments_node(self, arguments_names):
-        arguments_names_nodes = [ast_node.AstNode('name', value=argument_name) for argument_name in arguments_names]
-        arguments_arities_nodes = [ast_node.AstNode('arity', value=0)] * len(arguments_names)
-        arguments_nodes = [ast_node.AstNode('argument', children=list(argument)) for argument in zip(arguments_names_nodes, arguments_arities_nodes)]
-        return ast_node.AstNode('argument_list', children=arguments_nodes)
-
-    def _make_result_node(self, arity):
-        arity_node = ast_node.AstNode('arity', value=arity)
-        return ast_node.AstNode('result', children=[arity_node])
-
-    def _make_closure_body_node(self, call, arguments_names):
-        arguments_nodes = [ast_node.AstNode('IDENTIFIER', value=argument_name) for argument_name in arguments_names]
-        call.children[1].children.extend(arguments_nodes)
-        return ast_node.AstNode('entity_list', children=[call])
 
 if __name__ == '__main__':
     import function_type
@@ -115,9 +81,10 @@ if __name__ == '__main__':
     import preparser
 
     FUNCTIONS = {
-        '~': function_type.FunctionType(1),
-        '+': function_type.FunctionType(2),
-        'rand': function_type.FunctionType(1, function_type.FunctionType(1, function_type.FunctionType(1)))
+        '@': function_type.make_type([]),
+        '~': function_type.make_type([1]),
+        '+': function_type.make_type([2]),
+        'rand': function_type.make_type([1, 1, 1])
     }
 
     code = read_code.read_code()
