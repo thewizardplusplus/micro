953dd8f11a2aaab542b08aa6625ca2dedce3434f
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-08-05 15:58:07 +0300

    Fix the bug with an autounpacking of variables

--- micro/builtin_functions.py
+++ micro/builtin_functions.py
@@ -1,34 +1,35 @@
+import evaluate
 import function_type
 import math
 import random
 import string_utilities
 
 BUILTIN_FUNCTIONS = {
-    '!': function_type.make_type([1], handler=lambda x: not x),
-    '&&': function_type.make_type([2], handler=lambda x, y: x and y),
-    '||': function_type.make_type([2], handler=lambda x, y: x or y),
-    '==': function_type.make_type([2], handler=lambda x, y: x == y),
-    '!=': function_type.make_type([2], handler=lambda x, y: x != y),
-    '<': function_type.make_type([2], handler=lambda x, y: x < y),
-    '<=': function_type.make_type([2], handler=lambda x, y: x <= y),
-    '>': function_type.make_type([2], handler=lambda x, y: x > y),
-    '>=': function_type.make_type([2], handler=lambda x, y: x >= y),
-    '~': function_type.make_type([1], handler=lambda x: -x),
-    '+': function_type.make_type([2], handler=lambda x, y: x + y),
-    '-': function_type.make_type([2], handler=lambda x, y: x - y),
-    '*': function_type.make_type([2], handler=lambda x, y: x * y),
-    '/': function_type.make_type([2], handler=lambda x, y: x / y),
-    '%': function_type.make_type([2], handler=lambda x, y: x % y),
-    'floor': function_type.make_type([1], handler=lambda x: math.floor(x)),
-    'ceil': function_type.make_type([1], handler=lambda x: math.ceil(x)),
+    '!': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: not x)),
+    '&&': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x and y)),
+    '||': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x or y)),
+    '==': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x == y)),
+    '!=': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x != y)),
+    '<': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x < y)),
+    '<=': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x <= y)),
+    '>': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x > y)),
+    '>=': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x >= y)),
+    '~': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: -x)),
+    '+': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x + y)),
+    '-': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x - y)),
+    '*': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x * y)),
+    '/': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x / y)),
+    '%': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: x % y)),
+    'floor': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: math.floor(x))),
+    'ceil': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: math.ceil(x))),
     'random': function_type.make_type([], handler=lambda: random.random()),
     '$': function_type.make_type([], handler=lambda: ()),
-    ',': function_type.make_type([2], handler=lambda x, y: (x, y)),
-    'head': function_type.make_type([1], handler=lambda x: x[0]),
-    'tail': function_type.make_type([1], handler=lambda x: x[1]),
-    'if': function_type.make_type([3], handler=lambda condition, true, false: true if condition else false),
-    'str': function_type.make_type([1], handler=lambda x: string_utilities.string_to_list(str(x))),
-    'out': function_type.make_type([1], handler=lambda x: print(string_utilities.string_from_list(x)))
+    ',': function_type.make_type([2], handler=evaluate.make_unpacking_wrapper(lambda x, y: (x, y))),
+    'head': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: x[0])),
+    'tail': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: x[1])),
+    'if': function_type.make_type([3], handler=evaluate.make_unpacking_wrapper(lambda condition, true, false: true if condition else false)),
+    'str': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: string_utilities.string_to_list(str(x)))),
+    'out': function_type.make_type([1], handler=evaluate.make_unpacking_wrapper(lambda x: print(string_utilities.string_from_list(x))))
 }
 
 if __name__ == '__main__':
@@ -36,7 +37,6 @@ if __name__ == '__main__':
     import lexer
     import preparser
     import parser
-    import evaluate
 
     code = read_code.read_code()
     specific_lexer = lexer.Lexer()
--- micro/evaluate.py
+++ micro/evaluate.py
@@ -2,14 +2,17 @@ import utilities
 import string_utilities
 import function_type
 
-def evaluate(ast, functions={}):
+def evaluate(ast, functions={}, required_unpacking=True):
     result = None
     for entity in ast.children:
-        result = _evaluate_entity(entity, functions)
+        result = _evaluate_entity(entity, functions, required_unpacking)
 
     return result
 
-def _evaluate_entity(entity, functions):
+def make_unpacking_wrapper(handler):
+    return lambda *args: handler(*[_unpack(arg, True) for arg in args])
+
+def _evaluate_entity(entity, functions, required_unpacking):
     if entity.name == 'INTEGRAL_NUMBER':
         return int(entity.value)
     elif entity.name == 'REAL_NUMBER':
@@ -19,14 +22,14 @@ def _evaluate_entity(entity, functions):
     elif entity.name == 'STRING':
         return string_utilities.string_to_list(utilities.unquote(entity.value))
     elif entity.name == 'IDENTIFIER':
-        return _unpack(functions[entity.value])
+        return _unpack(functions[entity.value], required_unpacking)
     elif entity.name == 'function':
         return _evaluate_function(entity, functions)
     elif entity.name == 'call':
-        return _evaluate_call(entity, functions)
+        return _evaluate_call(entity, functions, required_unpacking)
 
-def _unpack(value):
-    if isinstance(value, function_type.FunctionType) and value.is_callable():
+def _unpack(value, required_unpacking):
+    if not required_unpacking or (isinstance(value, function_type.FunctionType) and value.is_callable()):
         return value
     else:
         return _trampoline(value)
@@ -55,14 +58,14 @@ def _make_function_handler(function_node, functions):
 
             functions[argument.children[0].value] = entity_type
 
-        return evaluate(function_node.children[1], functions)
+        return evaluate(function_node.children[1], functions, False)
 
     return handler
 
-def _evaluate_call(call, functions):
-    inner_function = _evaluate_entity(call.children[0].children[0].children[0], functions)
-    parameters = [_evaluate_entity(parameter, functions) for parameter in call.children[1].children]
-    return _unpack(inner_function(*parameters))
+def _evaluate_call(call, functions, required_unpacking):
+    inner_function = _evaluate_entity(call.children[0].children[0].children[0], functions, False)
+    parameters = [_evaluate_entity(parameter, functions, False) for parameter in call.children[1].children]
+    return _unpack(inner_function(*parameters), required_unpacking)
 
 if __name__ == '__main__':
     import read_code
@@ -72,12 +75,12 @@ if __name__ == '__main__':
 
     FUNCTIONS = {
         'ans': function_type.make_type([], handler=lambda: 42),
-        '~': function_type.make_type([1], handler=lambda x: -x),
-        '+': function_type.make_type([2], handler=lambda x, y: x + y),
-        '-': function_type.make_type([2], handler=lambda x, y: x - y),
-        '*': function_type.make_type([2], handler=lambda x, y: x * y),
-        '/': function_type.make_type([2], handler=lambda x, y: x / y),
-        '%': function_type.make_type([2], handler=lambda x, y: x % y)
+        '~': function_type.make_type([1], handler=make_unpacking_wrapper(lambda x: -x)),
+        '+': function_type.make_type([2], handler=make_unpacking_wrapper(lambda x, y: x + y)),
+        '-': function_type.make_type([2], handler=make_unpacking_wrapper(lambda x, y: x - y)),
+        '*': function_type.make_type([2], handler=make_unpacking_wrapper(lambda x, y: x * y)),
+        '/': function_type.make_type([2], handler=make_unpacking_wrapper(lambda x, y: x / y)),
+        '%': function_type.make_type([2], handler=make_unpacking_wrapper(lambda x, y: x % y))
     }
 
     code = read_code.read_code()
