64bf9642527c92ff0a7c6489edb7db95ce068778
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-08-11 14:17:57 +0300

    Add minimal tests

--- micro/_playground.py
+++ micro/_playground.py
@@ -1,5 +1,42 @@
 CODE = '''
-out str + 2 3
+fn outln(value)
+    out str value
+    out "\n"
+;
+
+outln + 2 3
+
+fn add(x y) + x y;
+outln add 2 3
+
+fn app(op:2 x y) op x y;
+outln app fn(x y) + x y; 2 3
+
+fn add(x):1 fn(y) + x y;;
+outln add 2 3
+
+fn ans() fn() 42;;
+outln ans
+
+fn add():0:2 fn():2 fn(x y) fn() + 2 3;;;;
+outln add 2 3
+
+fn loop(limit)
+    fn _loop(counter limit)
+        if < counter limit
+            fn()
+                out "\r"
+                out str counter
+
+                |>fn() _loop + 1 counter limit;
+            ;
+            fn();
+    ;
+
+    @ _loop 0 limit
+    out "\n"
+;
+loop 2000
 '''
 
 if __name__ == '__main__':
