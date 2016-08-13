d57800a587a4c82428a37d3729087229a1c890bb
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-08-13 04:00:54 +0300

    Extend the list of built-in functions

--- docs/todo.md
+++ docs/todo.md
@@ -5,30 +5,7 @@
 	* вызов замыканий из списка;
 	* возврат замыкания из функции с неверным (скалярным) типом возврата;
 * добавить функции:
-	* `nil():nil` &mdash; возвращает `None`;
 	* `args():list<str>` &mdash; возвращает список аргументов командной строки; первый элемент списка &mdash; путь к скрипту;
-	* `int(value:str):int` &mdash; парсит целое число из строки;
-	* `num(value:str):num` &mdash; парсит вещественное число из строки;
-	* `type(value:any):str` &mdash; возвращает имя типа переданного значения (`nil`, `int`, `num`, `list`, `pack` или `closure`);
-	* `arity(value:closure):list<int>` &mdash; возвращает описание типа замыкания в виде списка арностей (`FunctionType.to_array()`);
-	* математические функции:
-		* `trunc(number:int|num):int`;
-		* `sin(number:int|num):num`;
-		* `cos(number:int|num):num`;
-		* `tn(number:int|num):num`;
-		* `arcsin(number:int|num):num`;
-		* `arccos(number:int|num):num`;
-		* `arctn(number:int|num):num`;
-		* `arctn2(y:int|num, x:int|num):num`;
-		* `pow(base:int|num, exponent:int|num):num`;
-		* `sqrt(number:int|num):num`;
-		* `exp(number:int|num):num`;
-		* `ln(number:int|num):num`;
-		* `lg(number:int|num):num`;
-		* `abs(number:int|num):num`;
-	* `in():int` &mdash; читает один символ из stdin;
-	* `err(value:str):nil` &mdash; выводит строку в stderr;
-	* `exit(code:int):nil` &mdash; завершает программу с указанным кодом возврата;
 * добавить опции командной строки (сохранив логику загрузки скриптов):
 	* `-v` или `--version`;
 	* `-h` или `--help`;
--- micro/builtin_functions.py
+++ micro/builtin_functions.py
@@ -1,10 +1,42 @@
 import trampoline
+import type_utilities
 import function_type
+import string_utilities
+import functools
 import math
 import random
-import string_utilities
+import sys
+
+@trampoline.make_closure_trampoline_wrapper
+def _get_type_name(value):
+    name = ''
+    if value is None:
+        name = 'nil'
+    elif isinstance(value, int):
+        name = 'int'
+    elif isinstance(value, float):
+        name = 'num'
+    elif type_utilities.is_list(value):
+        name = 'list'
+    elif type_utilities.is_pack(value):
+        name = 'pack'
+    # you can't use the function type_utilities.is_closure(), because it's true only for 0-ary functions
+    elif isinstance(value, function_type.FunctionType):
+        name = 'closure'
+    else:
+        raise Exception("the unknown type " + value.__class__.__name__)
+
+    return string_utilities.make_list_from_string(name)
+
+@trampoline.make_closure_trampoline_wrapper
+def _get_closure_arity(value):
+    return functools.reduce(lambda pair, arity: (arity, pair), reversed(value.to_array()), ())
 
 BUILTIN_FUNCTIONS = {
+    'nil': function_type.make_type([], handler=lambda: None),
+    'num': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: float(string_utilities.make_string_from_list(x)))),
+    'type': function_type.make_type([1], handler=_get_type_name),
+    'arity': function_type.make_type([1], handler=_get_closure_arity),
     '!': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: not x)),
     '&&': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(lambda x, y: x and y)),
     '||': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(lambda x, y: x or y)),
@@ -22,6 +54,20 @@ BUILTIN_FUNCTIONS = {
     '%': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(lambda x, y: x % y)),
     'floor': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.floor)),
     'ceil': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.ceil)),
+    'trunc': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.trunc)),
+    'sin': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.sin)),
+    'cos': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.cos)),
+    'tn': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.tan)),
+    'arcsin': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.asin)),
+    'arccos': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.acos)),
+    'arctn': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.atan)),
+    'arctn2': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(math.atan2)),
+    'pow': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(math.pow)),
+    'sqrt': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.sqrt)),
+    'exp': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.exp)),
+    'ln': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.log)),
+    'lg': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.log10)),
+    'abs': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(math.fabs)),
     'random': function_type.make_type([], handler=random.random),
     '$': function_type.make_type([], handler=lambda: ()),
     ',': function_type.make_type([2], handler=trampoline.make_closure_trampoline_wrapper(lambda x, y: (x, y))),
@@ -32,7 +78,10 @@ BUILTIN_FUNCTIONS = {
     '<@': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda value: value[0])),
     '<<@': function_type.make_type([1], handler=trampoline.pack_trampoline),
     'str': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: string_utilities.make_list_from_string(string_utilities.get_representation(x)))),
-    'out': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: print(string_utilities.make_string_from_list(x), end='')))
+    'in': function_type.make_type([], handler=lambda: ord(sys.stdin.read(1))),
+    'out': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: print(string_utilities.make_string_from_list(x), end=''))),
+    'err': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: sys.stderr.write(string_utilities.make_string_from_list(x)))),
+    'exit': function_type.make_type([1], handler=trampoline.make_closure_trampoline_wrapper(lambda x: sys.exit(x)))
 }
 
 if __name__ == '__main__':
