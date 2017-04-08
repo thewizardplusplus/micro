e5e61188c4c21d4ecf22440e4da49f53c0d97e36
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-08 13:41:53 +0300

    Issue #2: add a support of an assignment to the parser

--- micro/utilities.py
+++ micro/utilities.py
@@ -7,6 +7,11 @@ def extract_function(function_node):
     result_type = function_type.make_type(function_node.children[0].children[2].children[0])
     return name, function_type.FunctionType(arity, result_type)
 
+def extract_assignment(assignment_node):
+    name = assignment_node.children[0].children[0].value
+    value_type = function_type.make_type(assignment_node.children[0].children[1])
+    return name, value_type
+
 def reduce_list(some_list, item_handler=None):
     items = [item_handler(item) if item_handler is not None else item for item in reversed(some_list)]
     return functools.reduce(lambda pair, item: (item, pair), items, ())
