25636f6181b85f942c5857a987a91298427c2c80
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-17 06:15:34 +0300

    Issue #17: correct the AstNode class

--- micro/ast_node.py
+++ micro/ast_node.py
@@ -1,4 +1,3 @@
-import ast_node_encoder
 import json
 
 class AstNode:
@@ -10,7 +9,18 @@ class AstNode:
             self.children = children
 
     def __str__(self):
-        return json.dumps(self, cls=ast_node_encoder.AstNodeEncoder)
+        return json.dumps(self, cls=AstNodeEncoder)
 
     def set_offset(self, offset):
         self.offset = offset
+
+class AstNodeEncoder(json.JSONEncoder):
+    def default(self, some_object):
+        if not isinstance(some_object, AstNode):
+            return super(AstNodeEncoder, self).default(some_object)
+
+        return {
+            key: some_object.__dict__[key]
+            for key in some_object.__dict__.keys()
+            if key != 'offset'
+        }
