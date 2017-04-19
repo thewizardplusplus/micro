b9b9501fba6f709fc859eafcf623af3fa386d8c3
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-19 10:31:10 +0300

    Issue #6: refactor the function type

--- micro/function_type.py
+++ micro/function_type.py
@@ -13,21 +13,27 @@ class FunctionType:
         return self.handler(*args)
 
     def is_callable(self):
-        return self.arity != 0 or ((self.result is not None) and self.result.is_callable())
+        return self.arity != 0 \
+            or (self.result is not None and self.result.is_callable())
 
     def get_result(self):
         return self.result if self.result is not None else FunctionType(0)
 
     def to_ast(self):
-        arities = self.to_array()
-        arities_nodes = [ast_node.AstNode('arity', value=arity) for arity in arities]
-        return ast_node.AstNode('type', children=arities_nodes)
+        return ast_node.AstNode(
+            'type',
+            children=[
+                ast_node.AstNode('arity', value=arity)
+                for arity in self.to_array()
+            ],
+        )
 
     def to_array(self):
         return _simplify_type(self._to_raw_array())
 
     def _to_raw_array(self):
-        return [self.arity] + (self.result._to_raw_array() if self.result is not None else [])
+        return [self.arity] \
+            + (self.result._to_raw_array() if self.result is not None else [])
 
 def make_type(value, handler=_default_handler):
     if isinstance(value, ast_node.AstNode):
@@ -36,11 +42,15 @@ def make_type(value, handler=_default_handler):
         return _make_type_from_array(value, handler)
 
 def _simplify_type(arities):
-    first_nonzero = next((index for index, arity in zip(range(len(arities), 0, -1), reversed(arities)) if arity != 0), -1)
-    if first_nonzero == -1:
-        return []
-
-    return arities[:first_nonzero]
+    # remove trailing zero arities
+    return arities[:next((
+        index
+        for index, arity in zip(
+            range(len(arities), 0, -1),
+            reversed(arities),
+        )
+        if arity != 0
+    ), 0)]
 
 def _make_type_from_ast(type_node, handler):
     arities = [int(arity_node.value) for arity_node in type_node.children]
@@ -53,4 +63,8 @@ def _make_type_from_raw_array(arities, handler=_default_handler):
     if len(arities) == 0:
         return FunctionType(0, handler=handler)
 
-    return FunctionType(arities[0], _make_type_from_raw_array(arities[1:]), handler)
+    return FunctionType(
+        arities[0],
+        _make_type_from_raw_array(arities[1:]),
+        handler,
+    )
