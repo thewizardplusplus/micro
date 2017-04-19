4b04f4ad1dd9d2e25c2dcdd04183aeaa036217d6
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-19 06:25:04 +0300

    Issue #6: refactor utilities

--- micro/utilities.py
+++ micro/utilities.py
@@ -3,10 +3,14 @@ import function_type
 def extract_function(function_node):
     name = function_node.children[0].children[0].value
     arity = len(function_node.children[0].children[1].children)
-    result_type = function_type.make_type(function_node.children[0].children[2].children[0])
+    result_type = function_type.make_type(
+        function_node.children[0].children[2].children[0],
+    )
     return name, function_type.FunctionType(arity, result_type)
 
 def extract_assignment(assignment_node):
     name = assignment_node.children[0].children[0].value
-    value_type = function_type.make_type(assignment_node.children[0].children[1])
+    value_type = function_type.make_type(
+        assignment_node.children[0].children[1],
+    )
     return name, value_type
