653fcc9c667a549ac2f7129e92345b1ec10f56fd
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-19 06:35:33 +0300

    Issue #6: refactor the trampoline

--- micro/trampoline.py
+++ micro/trampoline.py
@@ -7,9 +7,8 @@ def closure_trampoline(value):
     return value
 
 def make_closure_trampoline_wrapper(handler):
-    return lambda *args: handler(*[closure_trampoline(arg) for arg in args])
+    return lambda *args: handler(*list(map(closure_trampoline, args)))
 
-@make_closure_trampoline_wrapper
 def pack_trampoline(value):
     while type_utilities.is_pack(value):
         value = closure_trampoline(value[0])
