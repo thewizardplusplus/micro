917a2fdaa25e6f38543cb316cdd5fcf3b85266b8
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-23 12:02:57 +0300

    Issue #6: refactor the function type

--- micro/function_type.py
+++ micro/function_type.py
@@ -17,7 +17,7 @@ class FunctionType:
             or (self.result is not None and self.result.is_callable())
 
     def get_result(self):
-        return self.result if self.result is not None else FunctionType(0)
+        return self.result if self.result is not None else FunctionType()
 
     def to_ast(self):
         return ast_node.AstNode(
@@ -36,10 +36,13 @@ class FunctionType:
             + (self.result._to_raw_array() if self.result is not None else [])
 
 def make_type(value, handler=_default_handler):
+    arities = []
     if isinstance(value, ast_node.AstNode):
-        return _make_type_from_ast(value, handler)
-    else:
-        return _make_type_from_array(value, handler)
+        arities = [int(arity_node.value) for arity_node in value.children]
+    elif isinstance(value, list):
+        arities = value
+
+    return _make_type_from_raw_array(_simplify_type(arities), handler)
 
 def _simplify_type(arities):
     # remove trailing zero arities
@@ -52,16 +55,9 @@ def _simplify_type(arities):
         if arity != 0
     ), 0)]
 
-def _make_type_from_ast(type_node, handler):
-    arities = [int(arity_node.value) for arity_node in type_node.children]
-    return _make_type_from_array(arities, handler)
-
-def _make_type_from_array(arities, handler):
-    return _make_type_from_raw_array(_simplify_type(arities), handler)
-
 def _make_type_from_raw_array(arities, handler=_default_handler):
     if len(arities) == 0:
-        return FunctionType(0, handler=handler)
+        return FunctionType(handler=handler)
 
     return FunctionType(
         arities[0],
