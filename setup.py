f1a38247a9451b487fe16f3f84bd1b0e7d95665d
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-25 19:55:09 +0300

    Issue #7: add the setup.py script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import re
+
+package_name, *_ = setuptools.find_packages()
+with open('{}/utilities.py'.format(package_name), 'r') as utilities_file:
+    version = re.search(
+        r"^MICRO_VERSION = '([^']+)'$",
+        utilities_file.read(),
+        re.MULTILINE,
+    ).group(1)
+setuptools.setup(
+    name=package_name,
+    version=version,
+    license='MIT',
+    author='thewizardplusplus',
+    author_email='thewizardplusplus@yandex.ru',
+    url='https://github.com/thewizardplusplus/{}'.format(package_name),
+    packages=[
+        package_name,
+    ],
+    entry_points={
+        'console_scripts': [
+            '{0} = {0}:main'.format(package_name),
+        ],
+    },
+)
