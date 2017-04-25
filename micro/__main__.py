47d35605b02e4e68310f23529c560d5e3bf9a659
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-25 12:51:21 +0300

    Issue #52: process exceptions

--- micro/__main__.py
+++ micro/__main__.py
@@ -1,15 +1,20 @@
+import sys
+
 import options
 import builtin_functions
 import loading
 import utilities
 
-processed_options = options.process_options()
-loading.try_load_file(
-    processed_options.script,
-    {
-        **builtin_functions.BUILTIN_FUNCTIONS,
-        **options.make_args_function(processed_options),
-    },
-    processed_options.target,
-    utilities.get_base_path(processed_options.script),
-)
+try:
+    processed_options = options.process_options()
+    loading.try_load_file(
+        processed_options.script,
+        {
+            **builtin_functions.BUILTIN_FUNCTIONS,
+            **options.make_args_function(processed_options),
+        },
+        processed_options.target,
+        utilities.get_base_path(processed_options.script),
+    )
+except Exception as exception:
+    sys.exit('error: {}'.format(exception))
