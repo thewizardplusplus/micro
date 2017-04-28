0e3a0bc88fa18d8c04bb4a727f3d138200d23f45
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-28 14:40:09 +0300

    Issue #62: implement the ? function

--- micro/builtin_functions.py
+++ micro/builtin_functions.py
@@ -122,6 +122,10 @@ BUILTIN_FUNCTIONS = {
         [1],
         handler=lambda hash_: list_utilities.reduce_list(list(hash_.keys())),
     ),
+    '?': function_type.make_type(
+        [2],
+        handler=lambda value, default: value if value is not None else default,
+    ),
     'if': function_type.make_type(
         [3],
         handler=lambda condition, true, false: \
