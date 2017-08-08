4ef13c797759e38509d5ce05fc3112dced500d8b
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-08-08 09:29:13 +0300

    Issue #100: refactor a version parsing in the setup.py script

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 import setuptools
 import re
 import sys
+import os.path
 
 if not (0x030500f0 <= sys.hexversion < 0x040000a0):
     raise Exception('requires Python >=3.5, <4.0')
 
 package_name, *_ = setuptools.find_packages()
-with open('{}/utilities.py'.format(package_name), 'r') as utilities_file:
+with open(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        package_name,
+        'utilities.py',
+    ),
+    encoding='utf-8',
+) as utilities_file:
     version = re.search(
         r"^MICRO_VERSION = '([^']+)'$",
         utilities_file.read(),
         re.MULTILINE,
     ).group(1)
+
 setuptools.setup(
     name=package_name,
     version=version,
     license='MIT',
     author='thewizardplusplus',
     author_email='thewizardplusplus@yandex.ru',
-    url='https://github.com/thewizardplusplus/{}'.format(package_name),
+    url='https://github.com/thewizardplusplus/' + package_name,
     packages=[
         package_name,
     ],
