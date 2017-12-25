df188ac3e0e756c1d92ea3877c8d22806acc6585
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-12-25 15:12:16 +0300

    Issue #127: add the FileCache class

--- micro/file_cache.py
+++ micro/file_cache.py
@@ -15,3 +15,28 @@ class BaseFileLoader(abc.ABC):
         functions={},
     ):
         pass
+
+class FileCache:
+    def __init__(self, file_loader=None):
+        self._file_loader = file_loader
+        self._file_cache = {}
+
+    def get_file(self, base_path, local_base_path, filename, functions={}):
+        file_id = self._file_loader.make_file_id(
+            base_path,
+            local_base_path,
+            filename,
+        )
+        if file_id not in self._file_cache:
+            result = self._file_loader.load_file(
+                base_path,
+                local_base_path,
+                filename,
+                file_id,
+                functions,
+            )
+            self._file_cache[file_id] = result
+        else:
+            result = self._file_cache[file_id]
+
+        return result
