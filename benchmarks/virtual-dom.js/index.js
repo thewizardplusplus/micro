1e74fb8234461b746e38c7f4587a117c8b6d3f28
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-03 09:18:56 +0300

    Issue #151: create the virtual-dom package for JavaScript

---
+++ benchmarks/virtual-dom.js/index.js
@@ -0,0 +1,5 @@
+// https://medium.com/@deathmood/how-to-write-your-own-virtual-dom-ee74acc13060
+// https://medium.com/@deathmood/write-your-virtual-dom-2-props-events-a957608f5c76
+
+module.exports = {
+}
