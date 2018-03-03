6cf33865763a8c0d802398b4f0da52739ddd50e5
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-03 09:20:12 +0300

    Issue #151: add the make_node() function for JavaScript

--- benchmarks/virtual-dom.js/index.js
+++ benchmarks/virtual-dom.js/index.js
@@ -1,5 +1,10 @@
 // https://medium.com/@deathmood/how-to-write-your-own-virtual-dom-ee74acc13060
 // https://medium.com/@deathmood/write-your-virtual-dom-2-props-events-a957608f5c76
 
+function make_node(type, ...children) {
+  return {type, children}
+}
+
 module.exports = {
+  make_node,
 }
