303a56304288c90de6d320e8f600662d042ee522
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-03 09:41:15 +0300

    Issue #151: add the compare_nodes() function for JavaScript

--- benchmarks/virtual-dom.js/index.js
+++ benchmarks/virtual-dom.js/index.js
@@ -9,6 +9,27 @@ function make_difference(path, action, ...parameters) {
   return {path, action, parameters}
 }
 
+function compare_nodes(old_node, new_node, index=0, path=[]) {
+  if (!old_node) {
+    return [make_difference(path, 'create', new_node)]
+  } else if (!new_node) {
+    return [make_difference(path, 'remove', index)]
+  } else if (new_node.type !== old_node.type) {
+    return [make_difference(path, 'replace', index, new_node)]
+  } else {
+    let difference = []
+    const maximal_length = Math.max(old_node.children.length, new_node.children.length)
+    for (let i = 0; i < maximal_length; i++) {
+      const child_path = [...path, old_node.type]
+      const child_difference = compare_nodes(old_node.children[i], new_node.children[i], i, child_path)
+      difference.push(...child_difference)
+    }
+
+    return difference
+  }
+}
+
 module.exports = {
   make_node,
+  compare_nodes,
 }
