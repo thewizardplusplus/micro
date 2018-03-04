92911df560b99888ee1c11dd9c8b3ccb3d730190
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-04 13:26:40 +0300

    Issue #151: add the is_different() function for JavaScript

--- benchmarks/virtual-dom.js/index.js
+++ benchmarks/virtual-dom.js/index.js
@@ -9,14 +9,20 @@ function make_difference(path, action, ...parameters) {
   return {path, action, parameters}
 }
 
+function is_different(node_1, node_2) {
+  return typeof node_1 !== typeof node_2
+    || (typeof node_1 === 'string' && node_1 !== node_2)
+    || node_1.type !== node_2.type
+}
+
 function compare_nodes(old_node, new_node, index=0, path=[]) {
   if (!old_node) {
     return [make_difference(path, 'create', new_node)]
   } else if (!new_node) {
     return [make_difference(path, 'remove', index)]
-  } else if (new_node.type !== old_node.type) {
+  } else if (is_different(old_node, new_node)) {
     return [make_difference(path, 'replace', index, new_node)]
-  } else {
+  } else if (old_node.type) {
     let difference = []
     const maximal_length = Math.max(old_node.children.length, new_node.children.length)
     for (let i = 0; i < maximal_length; i++) {
@@ -26,6 +32,8 @@ function compare_nodes(old_node, new_node, index=0, path=[]) {
     }
 
     return difference
+  } else {
+    return []
   }
 }
 
