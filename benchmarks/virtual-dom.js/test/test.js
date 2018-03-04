29ad81a7d9ebd974b2ebdb9309724106cab1983c
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-04 16:10:29 +0300

    Issue #151: fix docs of unit tests in JavaScript

--- benchmarks/virtual-dom.js/test/test.js
+++ benchmarks/virtual-dom.js/test/test.js
@@ -1,7 +1,7 @@
 require('chai').should()
 const {make_node, compare_nodes} = require(process.env.TEST_SOURCE)
 
-describe('compare_nodes', () => {
+describe('compare_nodes()', () => {
   it("should create a node when there's no old node at same place", () => {
     const new_node = make_node('input')
     const difference = compare_nodes(undefined, new_node, 5, ['section', 'form'])
