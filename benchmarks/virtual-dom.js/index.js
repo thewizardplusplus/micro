78756856af4b0cc010894dcbc04cfc1496e89264
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-04 14:00:58 +0300

    Issue #151: refactor the compare_nodes() function in JavaScript

--- benchmarks/virtual-dom.js/index.js
+++ benchmarks/virtual-dom.js/index.js
@@ -16,25 +16,23 @@ function is_different(node_1, node_2) {
 }
 
 function compare_nodes(old_node, new_node, index=0, path=[]) {
+  let difference = []
   if (!old_node) {
-    return [make_difference(path, 'create', new_node)]
+    difference.push(make_difference(path, 'create', new_node))
   } else if (!new_node) {
-    return [make_difference(path, 'remove', index)]
+    difference.push(make_difference(path, 'remove', index))
   } else if (is_different(old_node, new_node)) {
-    return [make_difference(path, 'replace', index, new_node)]
+    difference.push(make_difference(path, 'replace', index, new_node))
   } else if (old_node.type) {
-    let difference = []
     const maximal_length = Math.max(old_node.children.length, new_node.children.length)
     for (let i = 0; i < maximal_length; i++) {
       const child_path = [...path, old_node.type]
       const child_difference = compare_nodes(old_node.children[i], new_node.children[i], i, child_path)
       difference.push(...child_difference)
     }
-
-    return difference
-  } else {
-    return []
   }
+
+  return difference
 }
 
 module.exports = {
