028da39adfb9aff689621e97a2291dc14fbf2b4d
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-03-17 02:45:36 +0300

    Issue #155: fix a path search

--- benchmarks/tools/benchmarker.bash
+++ benchmarks/tools/benchmarker.bash
@@ -3,11 +3,12 @@
 declare -r BOLD_FONT="$(tput bold)"
 declare -r RESET_FONT="$(tput sgr0)"
 
-declare -r script_path="$(dirname "$0")"
-declare -ri full_size=$(cat "$script_path/../index.js" | wc -m)
+declare -r base_path="$1"
+
+declare -ri full_size=$(cat "$base_path/index.js" | wc -m)
 echo "Size of the full version: $full_size B"
 
-declare -ri minified_size=$(cat "$script_path/../index.min.js" | wc -m)
+declare -ri minified_size=$(cat "$base_path/index.min.js" | wc -m)
 echo "Size of the minified version: $BOLD_FONT$minified_size B$RESET_FONT"
 
 declare -r saved="$(echo "scale=2; 100 - 100*$minified_size/$full_size" | bc)"
